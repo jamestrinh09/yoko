741e6255298d5ac8893e355b551bacb548e9fd85
Bella Vitale <team@dialedapps.com>
2026-07-11 11:00:10 -0700

    Add RevenueCat API keys to Config.swift

---
+++ ios-learnlock/Yoko/Config.swift
@@ -0,0 +1,13 @@
+test_zngoQynpZIqqsxkzoXmSrSaLzP//
+//  Config.swift
+//  Yoko
+//
+
+import Foundation
+
+enum Config {
+    static let EXPO_PUBLIC_REVENUECAT_TEST_API_KEY = "test_zngoQynpZIqqsxkzoXmSrSaLzPV"
+    static let EXPO_PUBLIC_REVENUECAT_IOS_API_KEY = "appl_bgzKjEqaVOpBcDBacNTfOetGzAh"
+    static let EXPO_PUBLIC_SUPABASE_URL = ""
+    static let EXPO_PUBLIC_SUPABASE_ANON_KEY = ""
+}
